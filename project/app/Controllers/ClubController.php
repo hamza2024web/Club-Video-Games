53d113e27dbd7d0f06329a3e89afef6e33c7853b
hamza2024web <hamzaakroubi1528@gmail.com>
2025-03-14 10:48:43 +0000

    add the section of event

--- project/app/Controllers/ClubController.php
+++ project/app/Controllers/ClubController.php
@@ -2,16 +2,22 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Services\ClubServices;
+use App\Services\ProfileServices;
+session_start();
 
 class ClubController extends BaseController {
     protected $clubServices;
+    protected $profileServices;
     public function __construct()
     {
         parent::__construct();
         $this->clubServices = new ClubServices();
+        $this->profileServices = new ProfileServices();
     }
     public function index (){
-        return $this->renderOrg('ClubManagement');
+        $user_id = $_SESSION["user_id"];
+        $profile = $this->profileServices->getProfileUSer($user_id);
+        return $this->renderOrg('ClubManagement',compact('profile'));
     }
 }
 ?>
