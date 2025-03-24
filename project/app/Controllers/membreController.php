fbc4706171054cd0ce8c96915e582a53e7d3fa70
hamza2024web <hamzaakroubi1528@gmail.com>
2025-03-24 09:48:39 +0000

    solve the problem of button modifier (the problem of js)

--- project/app/Controllers/membreController.php
+++ project/app/Controllers/membreController.php
@@ -1,16 +1,21 @@
 <?php
 namespace App\Controllers;
 use App\Controllers\BaseController;
-
+use App\Services\MembreServices;
+session_start();
 
 class membreController extends BaseController{
+    protected $MembreServices;
     public function __construct()
     {
         parent::__construct();
+        $this->MembreServices = new MembreServices();
     }
 
     public function dashboard (){
-        $this->renderMem('dashboard');
+        $user_id = $_SESSION["user_id"];
+        $member = $this->MembreServices->getProfileMembre($user_id);
+        $this->renderMem('dashboard',compact('member'));
     }
 
     
