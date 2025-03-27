2c48e5a6bba15e2d2672003021d506eff5404428
hamza2024web <hamzaakroubi1528@gmail.com>
2025-03-27 12:11:06 +0000

    fetch the games and fetch the profile informations

--- project/app/Controllers/JeuxController.php
+++ project/app/Controllers/JeuxController.php
@@ -1,17 +1,25 @@
 <?php
 namespace App\Controllers;
 use App\Controllers\BaseController;
-
+use App\Services\dashboardServices;
+use App\Services\ProfileServices;
+session_start();    
 
 class JeuxController extends BaseController{
-
+    protected $AdminServices ;
+    protected $ProfileServices;
     public function __construct()
     {
         parent::__construct();
+        $this->AdminServices = new dashboardServices();
+        $this->ProfileServices = new ProfileServices();
     }
 
     public function index(){
-        return $this->renderOrg('jeux');
+        $user_id = $_SESSION["user_id"];
+        $profile = $this->ProfileServices->getProfileUser($user_id);
+        $jeux = $this->AdminServices->getGame();
+        return $this->renderOrg('jeux',compact('jeux','profile'));
     }
 }
 ?>
