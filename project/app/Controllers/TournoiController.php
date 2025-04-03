4a981d0f78a6d7314183e44ad1384d1ae7f8f77d
hamza2024web <hamzaakroubi1528@gmail.com>
2025-04-03 23:41:38 +0000

    fetch the games into the form for creations the tournoi

--- project/app/Controllers/TournoiController.php
+++ project/app/Controllers/TournoiController.php
@@ -1,23 +1,27 @@
 <?php
 namespace App\Controllers;
 use App\Controllers\BaseController;
+use App\Services\JeuxServices;
 use App\Services\ProfileServices;
 session_start();
 
 class TournoiController extends BaseController{
     protected $TournnoiServices;
     protected $ProfileServices;
+    protected $JeuxServices;
     
     public function __construct()
     {
         parent::__construct();
         $this->ProfileServices = new ProfileServices();
+        $this->JeuxServices = new JeuxServices();
     }
 
     public function index(){
         $user_id = $_SESSION["user_id"];
         $profile = $this->ProfileServices->getProfileUser($user_id);
-        return $this->renderOrg('tournoi',compact('profile'));
+        $jeux = $this->JeuxServices->getGame($user_id);
+        return $this->renderOrg('tournoi',compact('profile','jeux'));
     }
 }
 ?>
