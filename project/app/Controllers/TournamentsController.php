01695c37ec6880b9630d0e7b37e017f7cd858d55
hamza2024web <hamzaakroubi1528@gmail.com>
2025-04-06 21:58:23 +0100

    fetch the member profile

--- project/app/Controllers/TournamentsController.php
+++ project/app/Controllers/TournamentsController.php
@@ -1,18 +1,24 @@
 <?php
 namespace App\Controllers;
 
+use App\Services\MembreServices;
 use App\Services\TournoiServices;
+session_start();
 
 class TournamentsController extends BaseController {
     protected $TournoiServices;
+    protected $MembreServices;
     public function __construct()
     {
         parent::__construct();
         $this->TournoiServices = new TournoiServices();
+        $this->MembreServices = new MembreServices();
     }
 
     public function index(){
-        return $this->renderMem('tournaments');
+        $user_id = $_SESSION["user_id"];
+        $member = $this->MembreServices->getProfileMembre($user_id);
+        return $this->renderMem('tournaments',compact('member'));
     }
 }
 ?>
