ea33bb3da1695d60ed00a58e8674753f7b0836a3
hamza2024web <hamzaakroubi1528@gmail.com>
2025-04-14 16:15:36 +0100

    count th enumber of participants for each evenement for organsateur but the transaction dsn't working

--- project/app/Repository/EvenementRepository.php
+++ project/app/Repository/EvenementRepository.php
@@ -15,9 +15,10 @@ class EvenementRepository {
     }
     public function fetchAllEvents($user_id){
         $sql = "SELECT evenement.id , evenement.name , evenement.date_debut , evenement.date_fin , evenement.lieu , evenement.statut ,evenement.description,evenement.type_evenement,evenement.numbre_membre,evenement.registration_start,evenement.registration_end,evenement.event_time,evenement.event_photo,evenement.entry_fee,evenement.requirements,evenement.discord_url,evenement.twitch_url,GROUP_CONCAT(evenement_programme.timeline_time),GROUP_CONCAT(evenement_programme.timeline_title),GROUP_CONCAT(evenement_programme.timeline_desc)
-        FROM evenement
+        , count(inscription_evenement.membre_id) as number_participants FROM evenement
         INNER JOIN club ON evenement.club_id = club.id
         INNER JOIN evenement_programme ON evenement_programme.event_id = evenement.id
+        LEFT JOIN inscription_evenement ON inscription_evenement.evenement_id = evenement.id
         INNER JOIN organisateur ON organisateur.club_id = evenement.club_id
         WHERE organisateur.user_id = :user_id
         GROUP BY evenement.id";
@@ -166,7 +167,7 @@ class EvenementRepository {
         $result = $stmt->execute();
         if ($result && $stmt->rowCount() > 0) {
             $sqlInscription = "DELETE FROM inscription_evenement WHERE evenement_id  = :event_id";
-            $stmt = $this->conn->pepare($sqlInscription);
+            $stmt = $this->conn->prepare($sqlInscription);
             $stmt->bindParam(":event_id",$event_id);
             $result = $stmt->execute();
             if ($result && $stmt->rowCount() > 0){
