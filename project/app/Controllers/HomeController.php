c986552ffffd34a7eaa5b554c690ee7f0bde8c25
hamza2024web <hamzaakroubi1528@gmail.com>
2025-04-22 15:09:07 +0100

    fetch the events to home page

--- project/app/Controllers/HomeController.php
+++ project/app/Controllers/HomeController.php
@@ -2,18 +2,23 @@
 namespace App\Controllers;
 
 use App\Services\dashboardServices;
+use App\Services\EventsMemberServices;
 
 class HomeController extends BaseController{
     protected $dashboardServices;
+    protected $EvenementServices;
 
     public function __construct()
     {
         parent::__construct();
         $this->dashboardServices = new dashboardServices();
+        $this->EvenementServices = new EventsMemberServices();
     }
+
     public function index(){
+        $events = $this->EvenementServices->getAllEvents();
         $games = $this->dashboardServices->getGame();
-        $home = $this->render('home',compact('games'));
+        $home = $this->render('home',compact('games','events'));
         return $home;
     }
 }
