ecf855717b9a81e99a8f4e41f734542d52314256
hamza2024web <hamzaakroubi1528@gmail.com>
2025-04-23 14:39:19 +0100

    fetch gmaes into dashoard memeber

--- project/app/Controllers/membreController.php
+++ project/app/Controllers/membreController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 use App\Controllers\BaseController;
+use App\Services\JeuxServices;
 use App\Services\MembreServices;
 use App\Services\StatistiqueAdminServices;
 
@@ -9,11 +10,13 @@ session_start();
 class membreController extends BaseController{
     protected $MembreServices;
     protected $StatistiqueAdminServices;
+    protected $JeuxServices;
     public function __construct()
     {
         parent::__construct();
         $this->MembreServices = new MembreServices();
         $this->StatistiqueAdminServices = new StatistiqueAdminServices();
+        $this->JeuxServices = new JeuxServices();
     }
 
     public function dashboard (){
@@ -23,7 +26,8 @@ class membreController extends BaseController{
         $notification = $this->MembreServices->CountNotifications($user_id);
         $Jeux_disponibles = $this->Jeux_disponibles($user_id);
         $tournoi_inscrit = $this->Tournoi($user_id);
-        $this->renderMem('dashboard',compact('member','my_solde','notification','Jeux_disponibles','tournoi_inscrit'));
+        $myGames = $this->JeuxServices->getGame($user_id);
+        $this->renderMem('dashboard',compact('member','my_solde','notification','Jeux_disponibles','tournoi_inscrit','myGames'));
     }
     public function Jeux_disponibles($user_id){
         $jeux = $this->StatistiqueAdminServices->GamesPurchase($user_id);
