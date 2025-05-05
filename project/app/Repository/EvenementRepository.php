1b7149f78709c64a6adc3371275d1240c2215bee
hamza2024web <hamzaakroubi1528@gmail.com>
2025-05-05 21:43:56 +0100

    solve the problem of count the number of inscription

--- project/app/Repository/EvenementRepository.php
+++ project/app/Repository/EvenementRepository.php
@@ -15,7 +15,7 @@ class EvenementRepository {
     }
     public function fetchAllEvents($user_id){
         $sql = "SELECT evenement.id , evenement.name , evenement.date_debut , evenement.date_fin , evenement.lieu , evenement.statut ,evenement.description,evenement.type_evenement,evenement.numbre_membre,evenement.registration_start,evenement.registration_end,evenement.event_time,evenement.event_photo,evenement.entry_fee,evenement.requirements,evenement.discord_url,evenement.twitch_url,GROUP_CONCAT(evenement_programme.timeline_time),GROUP_CONCAT(evenement_programme.timeline_title),GROUP_CONCAT(evenement_programme.timeline_desc)
-        , count(inscription_evenement.membre_id) as number_participants FROM evenement
+        , count(DISTINCT(inscription_evenement.membre_id)) as number_participants FROM evenement
         INNER JOIN club ON evenement.club_id = club.id
         INNER JOIN evenement_programme ON evenement_programme.event_id = evenement.id
         LEFT JOIN inscription_evenement ON inscription_evenement.evenement_id = evenement.id
